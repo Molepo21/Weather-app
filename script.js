6effeeae58e09a1d81725b3f6d89dcfd39c161cd
mehTro20 <justmondlisok@gmail.com>
2024-05-10 13:50:59 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function getWeather() {
-    const locationInput = document.getElementById('locationInput').value;
+    const locationInput = document.getElementById('location').value;
     const apiKey = 'd5b2c704e41b0060c220ef94c5b96aaa'; 
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${locationInput}&appid=${apiKey}&units=metric`;
 
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            const weatherInfo = document.getElementById('weatherInfo');
+            const weatherInfo = document.getElementById('weatherInformation');
             weatherInfo.innerHTML = `
                 <h2>Weather in ${data.name}</h2>
                 <p>Temperature: ${data.main.temp}°C</p>
@@ -16,6 +16,6 @@ function getWeather() {
             `;
         })
         .catch(error => {
-            console.error('Error fetching weather data:', error);
+            console.error('Could not fetch weather data:', error);
         });
 }
